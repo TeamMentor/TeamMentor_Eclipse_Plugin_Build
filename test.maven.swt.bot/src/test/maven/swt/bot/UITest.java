11d1f3fbe6b351437a5ec1ada258ff85276cd999
DinisCruz-QA <dinis.cruz+QA@owasp.org>
2013-10-18 18:10:52 +0100

    Made SWTWorkbenchBot static

--- test.maven.swt.bot/src/test/maven/swt/bot/UITest.java
+++ test.maven.swt.bot/src/test/maven/swt/bot/UITest.java
@@ -4,14 +4,29 @@ import static org.junit.Assert.*;
 
 import org.eclipse.swtbot.eclipse.finder.SWTEclipseBot;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class UITest {
 
+	protected static SWTWorkbenchBot bot;
+
+	@BeforeClass
+	public final static void beforeClass() throws Exception 
+	{
+		System.out.println("*** UI TESTS Executed via Maven");
+		bot = new SWTWorkbenchBot();
+		assertNotNull(bot);
+		try
+		{
+			bot.viewByTitle("Welcome").close();
+		}
+		catch(WidgetNotFoundException ex) {}
+	}
 	@Test
 	public void did_we_get_here() 
 	{
@@ -20,17 +35,12 @@ public class UITest {
 	@Test
 	public void SWTWorkbenchBot_OK() 
 	{
-		
-		SWTWorkbenchBot bot = new SWTWorkbenchBot();
 		assertNotNull(bot);
-		
 	}
 	@Test
 	public void SWTWorkbenchBot_ActionShell_Text() 
 	{
 		
-		SWTWorkbenchBot bot = new SWTWorkbenchBot();
-		assertNotNull(bot);
 		SWTBotShell shell = bot.activeShell();
 		
 		String text = shell.getText();
@@ -39,6 +49,11 @@ public class UITest {
 		SWTBotShell[] shells = bot.shells();
 		
 		assertNotNull(shells);
+	}
+	
+	public void SWTWorkbenchBot_ActionShell_OpenProject() 
+	{
+			
 	}
 
 }
