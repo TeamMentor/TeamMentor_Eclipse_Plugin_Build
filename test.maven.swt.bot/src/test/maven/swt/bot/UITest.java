fa008fc2258f1124c199c81588d19ff3d20d6530
DinisCruz-Dev <dinis.cruz+dev@owasp.org>
2013-10-22 12:20:41 +0100

    added test Check_that_ProjectExplorer_Is_Avaialble()

--- test.maven.swt.bot/src/test/maven/swt/bot/UITest.java
+++ test.maven.swt.bot/src/test/maven/swt/bot/UITest.java
@@ -2,13 +2,13 @@ package test.maven.swt.bot;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotButton;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
@@ -40,5 +40,13 @@ public class UITest {
 	{
 		assertNotNull(bot);
 		bot.captureScreenshot("screenshots/SWTWorkbenchBot_OK.jpeg");
+	}
+	
+	@Test
+	public void Check_that_ProjectExplorer_Is_Avaialble()
+	{
+		List<SWTBotView> views = bot.views(WidgetMatcherFactory.withPartName("Project Explorer"));
+		assertNotNull(views);
+		assertEquals(views.size(), 1);
 	}
 }
